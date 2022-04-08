2750f3de32124b365a1efde4acd977162bf6b0fc
SaronTeb <saripari61@gmail.com>
2022-04-08 04:09:01 -0700

    lowlevel

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- *main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - This prints the line
+ * Return: returns the line
  */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
-
