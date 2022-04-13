8d53d17246dfa1fb65c1ca45eba713cec5654dca
SaronTeb <saripari61@gmail.com>
2022-04-12 23:32:13 -0700

    updated

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,15 @@
-#include"main.h"
-
+#include "main.h"
 /**
- * _isalpha - checks if character is a letter
- *            both lowercase or uppercase
- *
- * @c: takes input from other functions.
- *
- * Return: 1 is c if true else 0
+ * _isalpha - Check if character is a alphabet character.
+ * @c: type int character
+ * Return: 1 if letter, lowercase or uppercase, and 0 otherwise
  */
 
 int _isalpha(int c)
 {
-  if (c >= 97 && c <= 122 &&
-      c >= 65 && c >= 90)
-return (1);
-return (0);
+  if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    return (1);
+
+  else
+    return (0);
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,19 +12,19 @@
 
 int print_sign(int n)
 {
-  if (n > 0)
-    {
-      _putchar(43 + 0);
-      return (1);
-    }
-  else if (n == 0)
-    {
-      _putchar(0 + 48);
-      return (0);
-    }
-  else
-    {
-      _putchar(45 + 0);
-      return (-1);
-    }
+if (n > 0)
+{
+_putchar(43 + 0);
+return (1);
+}
+else if (n == 0)
+{
+_putchar(0 + 48);
+return (0);
+}
+else
+{
+_putchar(45 + 0);
+return (-1);
+}
 }
