e678fe629c83461ee3b459ba69bd686aaecc6b65
SaronTeb <saripari61@gmail.com>
2022-05-29 05:01:05 -0700

    bitmanipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,39 +1,37 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: b is pointing to a string of 0 and 1 chars
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
  *
- * Return: Converted number or 0 if
- * there is one or more chars in the string b that is not 0 or 1
- * or if b is NULL
+ * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0, one, i;
-	int l = 0;
+	unsigned int ui;
+	int len, base_two;
 
-	if (b == 0)
+	if (!b)
 		return (0);
 
+	ui = 0;
 
-	while (b[l] != 0)
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[l] != '0' && b[l] != '1')
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
-		l++;
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
 
-	l = l - 1;
-
-	for (i = 0; l >= 0; i++, l--)
-	{
-		if (b[l] == '0')
-			one = 0;
-		else if (b[l] == '1')
-			one = 1;
-		one = one << i;
-		res = res | one;
-	}
-	return (res);
+	return (ui);
 }
